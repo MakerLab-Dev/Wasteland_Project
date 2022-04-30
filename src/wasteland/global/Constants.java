2f499fb3b0740cef792d14b4445008a0e72ec026
MakerLab-Dev <47223013+MakerLab-Dev@users.noreply.github.com>
2022-04-30 22:01:32 +0200

    Game constants

---
+++ src/wasteland/global/Constants.java
@@ -0,0 +1,10 @@
+package wasteland.global;
+
+public class Constants {
+    // FPS is the frames per second.
+    public static final int fps = 10;
+    // ViewWidth is the width of the view in tiles.
+    public static final int viewWidth = 20;
+    // ViewHeight is the height of the view in tiles.
+    public static final int viewHeight = 20;
+}
