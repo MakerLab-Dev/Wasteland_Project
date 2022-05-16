8c544d1d331a05d69a4d79251151ce5ee084098e
Space Cowboy <space_cowboy@pop-os.localdomain>
2022-05-16 12:33:59 +0200

    Collisions class

---
+++ src/wasteland/entity/Collisions.java
@@ -0,0 +1,24 @@
+package wasteland.entity;
+
+import wasteland.items.Item;
+import wasteland.renderer.*;
+import wasteland.world.*;
+
+public abstract class Collisions {
+    boolean checkCollision(int x, int y, World world, Entity entity) {
+        // Check world bounds
+        
+
+        // Check tile collision
+        
+
+        // Check entity collision
+        
+
+        // Check item collision
+        
+
+        // No collision
+        return false;
+    }
+}
